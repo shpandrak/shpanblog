4f5a8c6a06d953f593d402550e4d4d850e104e4f
Amit Lieberman <amit.lieberman@solaredge.com>
2025-08-29 07:27:32 +0300

    Temporarily disable comments

--- tools/comentario/buildImage.sh
+++ tools/comentario/buildImage.sh
@@ -1,3 +1,3 @@
-docker build -t comentario-with-shh .
-docker tag comentario-with-shh me-west1-docker.pkg.dev/shpankids/my-docker-repo/comentario:v3.14.0-right
-docker push  me-west1-docker.pkg.dev/shpankids/my-docker-repo/comentario:v3.14.0-right
+docker build -t comentario-with-add-secrets .
+docker tag comentario-with-add-secrets me-west1-docker.pkg.dev/shpankids/my-docker-repo/comentario:v3.14.0-add-secrets
+docker push  me-west1-docker.pkg.dev/shpankids/my-docker-repo/comentario:v3.14.0-add-secrets
